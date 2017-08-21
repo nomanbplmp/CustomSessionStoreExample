80547937c28c292d13c3a7ec1c4537391c3d1f5b
nomanbplmp <nomanbplmp@yaho.co.in>
2017-08-22 00:23:32 +0530

    separated session config

---
+++ src/main/java/com/nk/test/spring/session/config/SessionConfig.java
@@ -0,0 +1,36 @@
+package com.nk.test.spring.session.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.session.jdbc.JdbcOperationsSessionRepository;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+import org.springframework.transaction.PlatformTransactionManager;
+
+
+
+@Configuration
+@EnableJdbcHttpSession
+class SessionConfig { 
+	@Bean
+	public JdbcOperationsSessionRepository sessionRepository(){
+	  DataSource ds =	DataSourceBuilder.create().driverClassName("org.h2.Driver").username("sa").url("jdbc:h2:file:~/test").build();
+	 return	  new SessionRepo(ds,new DataSourceTransactionManager(ds));
+	 
+	}
+}
+
+
+class SessionRepo extends JdbcOperationsSessionRepository  {
+
+	public SessionRepo(DataSource dataSource, PlatformTransactionManager transactionManager) {
+		super(dataSource, transactionManager);
+		
+	}
+	
+	
+}
+
